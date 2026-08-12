fcafbd68f72b291d6c9ec9a1dfce42dacd9433a5
PranathiReganti <pranathireganti@gmail.com>
2026-08-12 22:51:10 +0530

    Fix backend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://fieldsight-vwq1.onrender.com";
+const API_URL = "https://fieldsight-wwq1.onrender.com"; 
 
 function App() {
   const [file, setFile] = useState(null);
